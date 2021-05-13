7e17e91b0d591eec24e546ffad992d004e3e0a0d
Srta Katsumi <73000702+srtakatsumi@users.noreply.github.com>
2021-05-13 18:57:25 -0300

    Create jogo.py

---
+++ jogo.py
@@ -0,0 +1,53 @@
+import random #importa a biblioteca este módulo implementa geradores de números pseudoaleatórios para várias distribuições
+
+print("*" * 20)
+print("Bem vindo ao jogo de Adivinhação!") #imprime a mensagem entre aspas para o usuário
+print("*" * 20)
+
+numero_secreto = random.randrange(1, 101) #variavel criada para gera aleatoriamente um número inteiro dentro de um intervalo dado pelo usuário
+total_de_tentativas = 0#variavel zerada será atribudo valor depois
+pontos = 1000 #variavel pontos que o usuário irá ter
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: ")) #variavel gerada para guardar o valor inserido pelo usuário de qual nivel deseja
+
+if(nivel == 1):  #validação da escolha do nível
+    total_de_tentativas = 20 #total de tentativas que o jogador terá de acordo com cada nivel 
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    
+    chute_str = input("Digite um número entre 1 e 100: ") #chute será um resultado em string
+    print("Você digitou ", chute_str)
+    chute = int(chute_str)
+    
+    if(chute < 1 or chute > 100): #valida se o chute do usuário está dentro dos números aleatórios gerados
+         print("Você deve digitar um número entre 1 e 100!")
+         continue
+         
+    acertou = chute == numero_secreto #atribui que chute ao acerto, ou seja, chute é igual numero_secreto? Se sim vai exibir o número de pontos e informar que acertou
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+    
+    if(acertou):
+         print("Você acertou e fez {} pontos!".format(pontos))
+         break #se o usuário acertou o break encerra o programa
+         
+         pontos_perdidos = abs(numero_secreto - chute)
+         pontos = pontos - ponto_perdidos
+         if(maior):
+            print("O seu chute foi maior que o número secreto")
+            if (rodada == total_tentativas):
+                print("O número secreto era {}. Você fez {}".format(numero_secreto, pontos))
+         elif(menor):
+             print("Você errou! O seu chute foi menor do que o número secreto.")
+             if (rodada == total_tentativas): #valida se total de rodadas realizadas é igual ao total de tentativas estipuladas
+                 print("O número secreto era {}. Você fez {}".format(numero_secreto, pontos)) #utilizamos os colchetes em branco para sinalizar as lacunas que iremos preenchere
+                
+print("Fim do jogo!")  #sinalizamos ao jogador que o jogo acabou 
